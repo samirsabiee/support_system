ae044d80b28638563df0705aaf8799e984335e54
samsab1995 <samirsabiee@gmail.com>
2021-09-02 17:57:03 +0430

    remove extra import

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -14,7 +13,6 @@ class Admin extends Authenticatable
 
     /**
      * The attributes that are mass assignable.
-     *
      * @var string[]
      */
     protected $fillable = [
