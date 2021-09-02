ee5705c8aeb9e463f25d96fd8c404665d6616b32
samsab1995 <samirsabiee@gmail.com>
2021-09-02 20:46:54 +0430

    show tickets blade

---
+++ resources/views/tickets/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="d-flex flex-column justify-content-center align-items-center">
+        <table class="col-8 table table-bordered">
+            <thead>
+            <tr>
+                <th>Title</th>
+                <th>User</th>
+                <th>Priority</th>
+                <th>Status</th>
+                <th>Make Date</th>
+            </tr>
+            </thead>
+            <tbody>
+            @forelse($tickets as $ticket)
+                <tr>
+                    <td>{{ $ticket->title }}</td>
+                    <td>{{ $ticket->user->name }}</td>
+                    <td>{{ $ticket->priority }}</td>
+                    <td>{{ $ticket->status }}</td>
+                    <td>{{ $ticket->created_at }}</td>
+                </tr>
+            @empty
+                <p>No Tickets</p>
+            @endforelse
+            </tbody>
+        </table>
+    </div>
+@endsection
