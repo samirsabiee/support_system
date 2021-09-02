6284193dff07fd19a0e5766626db3a9be573d2c9
samsab1995 <samirsabiee@gmail.com>
2021-09-03 01:10:33 +0430

    register ReplyCreated event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\ReplyCreated;
+use App\Listeners\ChangeStatusTicket;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -15,8 +17,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        ReplyCreated::class => [
+            ChangeStatusTicket::class,
         ],
     ];
 
