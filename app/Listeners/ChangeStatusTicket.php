aefa8a403548706cd4f2352d94c15391242fc44c
samsab1995 <samirsabiee@gmail.com>
2021-09-03 01:09:04 +0430

    create ReplyCreated.php event and ChangeStatusTicket.php listener

---
+++ app/Listeners/ChangeStatusTicket.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ReplyCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class ChangeStatusTicket
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  ReplyCreated  $event
+     * @return void
+     */
+    public function handle(ReplyCreated $event)
+    {
+        if($event->reply->ticket->isCreated() && $event->user->isAdmin()) {
+            $event->reply->ticket->replied();
+        }
+    }
+}
