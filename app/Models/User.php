4160682d467aec9af5036ac8a54b2066c6d3f612
samsab1995 <samirsabiee@gmail.com>
2021-09-03 01:12:36 +0430

    add isAdmin func

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,9 @@ class User extends Authenticatable
     {
         return $this->morphMany(Reply::class, 'repliable');
     }
+
+    public function isAdmin(): bool
+    {
+        return $this instanceof Admin;
+    }
 }
