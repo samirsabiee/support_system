78278e73f637cc82a67229ae8b1454cc1bbd9149
samsab1995 <samirsabiee@gmail.com>
2021-09-02 15:31:39 +0430

    complete login and register

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,58 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Models\Admin;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
-    //
+    use AuthenticatesUsers;
+
+    protected string $redirectTo = 'home';
+
+    /**
+     * AdminController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:web');
+        $this->middleware('guest:admin');
+    }
+
+    public function showRegisterForm()
+    {
+        return view('admin.register');
+    }
+
+    public function register(RegisterRequest $request): RedirectResponse
+    {
+        $admin = $this->create($request->all());
+        $this->guard()->login($admin);
+        return redirect($this->redirectTo);
+    }
+
+    private function create(array $data)
+    {
+        return Admin::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'department' => $data['department']
+        ]);
+    }
+
+    protected function guard()
+    {
+        return Auth::guard('admin');
+    }
+
+    public function showLoginForm()
+    {
+        return view('admin.login');
+    }
 }
