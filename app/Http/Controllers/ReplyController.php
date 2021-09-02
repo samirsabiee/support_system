88f656ce7bdf53f41a22dc57fd3c264380749f72
samsab1995 <samirsabiee@gmail.com>
2021-09-02 22:38:14 +0430

    add funcs needed to reply

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ReplyController extends Controller
 {
-    //
+
+    /**
+     * ReplyController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:web,admin');
+    }
+
+    public function create(Ticket $ticket, Request $request): RedirectResponse
+    {
+        auth()->user()->replies()->create([
+            'text' => $request->text,
+            'ticket_id' => $ticket->id,
+        ]);
+        return back();
+    }
 }
