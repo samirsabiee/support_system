b57262d198a523312db6e6b0f7809a6201849e3c
samsab1995 <samirsabiee@gmail.com>
2021-09-02 15:30:36 +0430

    add guard and fillable

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+    protected string $guard = 'admin';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'department',
+    ];
 }
