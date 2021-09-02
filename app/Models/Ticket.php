1badc2ab4154de2a3797feb9c15c0d12af78a93d
samsab1995 <samirsabiee@gmail.com>
2021-09-02 20:47:11 +0430

    add user relation

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Ticket extends Model
 {
@@ -14,4 +15,9 @@ class Ticket extends Model
     protected $attributes = [
         'status' => 0,
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
